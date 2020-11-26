1ae01b5d29a18ac8c49ada758cae3123afaa7415
Kuludu <i@kuludu.net>
2020-11-26 11:06:25 +0800

    Update Quote

--- app/src/main/java/net/kuludu/smartrecite/Quote.java
+++ app/src/main/java/net/kuludu/smartrecite/Quote.java
@@ -11,15 +11,15 @@ public class Quote {
         this.chinese = chinese;
     }
 
-    public void setIndex(Integer index) {
-        this.index = index;
+    public Integer getIndex() {
+        return index;
     }
 
-    public void setEnglish(String english) {
-        this.english = english;
+    public String getEnglish() {
+        return english;
     }
 
-    public void setChinese(String chinese) {
-        this.chinese = chinese;
+    public String getChinese() {
+        return chinese;
     }
 }
